c6ad7dd6e4dc058f0e51c7050d3c39ccbc3998a6
vuquyet8080 <vuquyet8080@gmail.com>
2021-01-08 16:30:29 +0700

    add antd library

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Skill from "./components/Skill";
 import Experiences from "./components/Experiences";
 import Education from "./components/Education";
 import "./index.css";
+import { Button, Radio } from "antd";
+import "antd/dist/antd.css";
 function App() {
   return (
     <div>
-      <Header />
+      {/* <Header />
       <AboutMe />
       <Skill />
       <Experiences />
-      <Education />
+      <Education /> */}
+      <Button size={14}>Default</Button>
     </div>
   );
 }
